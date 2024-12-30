e18a62bace3ace9bb45905269b544df023f499bc
unknown <mouad.aitnaim@gmail.com>
2024-12-30 10:41:06 +0100

    Update routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import {
   createRouter,
-  createWebHistory,
+  createWebHashHistory,
 } from 'vue-router';
 
 import { useAuthStore } from '../../core/Data/stores/auth';
@@ -131,7 +131,7 @@ const routes = [
 
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
